57cf0bba7bad1368b41a07a2c90c8235e273401b
yusam <yusam@yandex.ru>
2023-08-04 10:20:47 +0300

    #changed

--- config/config.example.php
+++ config/config.example.php
@@ -3,7 +3,7 @@
 return [
     'testing' => [
         'isDebugging' => true,
-        'baseUrl' => 'http://localhost:9081',
+        'baseUrl' => 'http://localhost',
         'storageLogFile' => __DIR__ . '/../tmp/curl-ext-debug.log',
         "userId" => 0,
         "privateKey" => ""
