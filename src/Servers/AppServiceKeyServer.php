a57733dc80163dd154f0d88a8ac206b024e2119e
yusam <yusam@yandex.ru>
2023-10-28 00:00:41 +0300

    #changed

--- src/Servers/AppServiceKeyServer.php
+++ src/Servers/AppServiceKeyServer.php
@@ -5,18 +5,18 @@ namespace YusamHub\Project0001ClientAuthSdk\Servers;
 use YusamHub\Project0001ClientAuthSdk\Exceptions\JsonAuthRuntimeException;
 class AppServiceKeyServer extends BaseTokenServer
 {
-    protected string $serviceKey;
+    protected ?string $serviceKey;
     protected ?string $token;
     protected ?string $sign;
     protected ?string $content;
 
     /**
-     * @param string $serviceKey
+     * @param string|null $serviceKey
      * @param string|null $token
      * @param string|null $sign
      * @param string|null $content
      */
-    public function __construct(string $serviceKey,
+    public function __construct(?string $serviceKey,
                                 ?string $token,
                                 ?string $sign,
                                 ?string $content)
@@ -32,6 +32,16 @@ class AppServiceKeyServer extends BaseTokenServer
      */
     public function authorizeOrFail(): void
     {
+        if (empty($this->serviceKey)) {
+            throw new JsonAuthRuntimeException([
+                self::TOKEN_KEY_NAME => 'Service key not defined',
+            ]);
+        }
+        if (empty($this->token)) {
+            throw new JsonAuthRuntimeException([
+                self::TOKEN_KEY_NAME => 'Token not defined',
+            ]);
+        }
         if ($this->token != $this->serviceKey) {
             throw new JsonAuthRuntimeException([
                 self::TOKEN_KEY_NAME => 'Invalid value',
